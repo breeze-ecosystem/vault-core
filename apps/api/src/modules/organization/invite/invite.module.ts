231e35112467925cdd966e273d56d5fbdda04f2a
breeze-ecosystem <breezeecosystem@gmail.com>
2026-07-16 14:34:35 +0000

    fix: InviteModule needs AuthModule (forwardRef for circular dep)

--- apps/api/src/modules/organization/invite/invite.module.ts
+++ apps/api/src/modules/organization/invite/invite.module.ts
@@ -1,8 +1,10 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
+import { AuthModule } from "../../auth/auth.module";
 import { InviteController } from "./invite.controller";
 import { InviteService } from "./invite.service";
 
 @Module({
+  imports: [forwardRef(() => AuthModule)],
   controllers: [InviteController],
   providers: [InviteService],
   exports: [InviteService],
