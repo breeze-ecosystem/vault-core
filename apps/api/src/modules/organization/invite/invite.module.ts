df0136543d0231ee2ebd30f3a990740c75d550da
breeze-ecosystem <breezeecosystem@gmail.com>
2026-07-16 15:10:05 +0000

    fix: add JwtModule to InviteModule imports - needed for JwtService

--- apps/api/src/modules/organization/invite/invite.module.ts
+++ apps/api/src/modules/organization/invite/invite.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 import { InviteController } from "./invite.controller";
 import { InviteService } from "./invite.service";
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [InviteController],
   providers: [InviteService],
   exports: [InviteService],
